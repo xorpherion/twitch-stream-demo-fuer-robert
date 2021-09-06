571145a849f12ea366b87b8df3861c3429aa9e86
Till Born <xorpherion@users.noreply.github.com>
2021-09-06 22:49:09 +0200

    Update Program.cs
    translate english literals to german

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace RobertGitProjekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hallo Welt!");
         }
     }
 }
